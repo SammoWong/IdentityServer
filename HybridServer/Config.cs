84bb89e89e2b99f5d5ca7fcdf9d45ad68104d852
Sammo <sammo.wong@foxmail.com>
2018-04-18 16:26:12 +0800

    add JavaScriptClient

--- HybridServer/Config.cs
+++ HybridServer/Config.cs
@@ -42,6 +42,26 @@ namespace HybridServer
                     },
                     AllowOfflineAccess = true,
                     AllowAccessTokensViaBrowser = true
+                },
+
+                // JavaScript Client
+                new Client
+                {
+                    ClientId = "js",
+                    ClientName = "JavaScript Client",
+                    AllowedGrantTypes = GrantTypes.Implicit,
+                    AllowAccessTokensViaBrowser = true,
+
+                    RedirectUris =           { "http://localhost:5003/callback.html" },
+                    PostLogoutRedirectUris = { "http://localhost:5003/index.html" },
+                    AllowedCorsOrigins =     { "http://localhost:5003" },
+
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "hybrid_api"
+                    }
                 }
             };
         }
